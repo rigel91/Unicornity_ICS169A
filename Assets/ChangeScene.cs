33ef07ab6dad62bbb4ca500428aa40c415d57714
Daniel Alcala <dalcala94@gmail.com>
2020-10-23 00:19:02 -0700

    Create ChangeScene.cs

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+   public void ChangeToScene (string sceneToChangeTo)
+    {
+        //SceneManager.LoadScene(sceneToChangeTo); Use once more scenes develop
+        //For now just for testing purposes.
+        print("I WORK!");
+    }
+}
