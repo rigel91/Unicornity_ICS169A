10f8b63765cc6224d1c4abe15135b1fc2a1f4fd2
Erol Boyar <eboyar@uci.edu>
2020-10-27 12:57:14 -0700

    developed a basic tutorial puzzle
    developed the text boxes to send more than just one message (and loop dialog) and created a scene with dialog based on our sample puzzle slide

--- Assets/dialogBox.cs
+++ Assets/dialogBox.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class dialogBox : MonoBehaviour
 {
-    public string dialog;
+    public string Dialog1;
+    public string Dialog2;
+    public string Dialog3;
+    public string Dialog4;
+    public string Dialog5;
+    public string Dialog6;
+    public string Dialog7;
+    public string Dialog8;
+    public string Dialog9;
+    public string Dialog10;
+
+    private List<string> AllDialog;
+    private string currentDialog;
+    private int dialogIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        AllDialog = new List<string> { Dialog1, Dialog2, Dialog3, Dialog4, Dialog5, Dialog6, Dialog7, Dialog8, Dialog9, Dialog10 };
+        currentDialog = AllDialog[0];
     }
 
     // Update is called once per frame
@@ -18,8 +32,33 @@ public class dialogBox : MonoBehaviour
 
     }
 
+    private string GetCurrentDialog()
+    {
+        return currentDialog;
+    }
+
+    private void advanceDialog()
+    {
+        dialogIndex += 1;
+        if (dialogIndex >= AllDialog.Count)
+        {
+            dialogIndex = 0;
+        }
+        else if (AllDialog[dialogIndex].Length <= 0)
+        {
+            dialogIndex = 0; //if reached end of dialog, loop back to first message
+        }
+
+        currentDialog = AllDialog[dialogIndex];
+
+    }
+
+
     public string RequestDialog()
     {
-        return dialog;
+        string returnDialog = GetCurrentDialog();
+        advanceDialog();
+        return returnDialog;
+
     }
 }
