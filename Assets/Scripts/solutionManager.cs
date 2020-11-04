9dff363b72329214c5588574be5a692efc0e00cb
Erol Boyar <eboyar@uci.edu>
2020-11-04 15:18:00 -0800

    solution checker for puzzles
    created a solution checking system for the draggable word objects so that words can be assigned a correct space in the notebook and once all of a certain set of words are all in their correct spots, they lock in place and turn green to show confirmation

---
+++ Assets/Scripts/solutionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class solutionManager : MonoBehaviour
+{
+    public solutionBox[] solutionBoxes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void checkAllSolutions()
+    {
+        bool allCorrect = true;
+        foreach (solutionBox box in solutionBoxes)
+        {
+            if (!box.isCorrect())
+            {
+                allCorrect = false;
+            }
+        }
+        if (allCorrect)
+        {
+            foreach (solutionBox box in solutionBoxes)
+            {
+                box.lockPosition();
+            }
+            Debug.Log("puzzle solved!");
+        }
+    }
+
+    
+}
