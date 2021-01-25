79503b6a63867b4e9e4f851535236bde09b65bb0
Rigel Kuper <rkuper@uci.edu>
2021-01-24 23:34:20 -0800

    added new doctor building

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,35 +4,28 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;
+    private Transform target;
     private moveplayer move;
-
-    public float xOffset;
-    [Range(1, 10)]
-    public float smoothness;
-
-    //set bounds for the camera
-    public float leftBound;
-    public float rightBound;
-    public float topBound;
-    public float bottomBound;
-
+    private GameManager gm;
     //camera duplicates
     private static bool cameraExists;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!cameraExists)
-        {
-            cameraExists = true;
-            DontDestroyOnLoad(transform.gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        gm = FindObjectOfType<GameManager>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        
+
+        //if (!cameraExists)
+        //{
+        //    cameraExists = true;
+        //    DontDestroyOnLoad(transform.gameObject);
+        //}
+        //else
+        //{
+        //    Destroy(gameObject);
+        //}
 
         move = target.GetComponent<moveplayer>();
     }
@@ -40,37 +33,22 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 targetPosition = target.position + offset;
+        Vector3 targetPosition = target.position + gm.offset;
         if (move.translation == 0)
         {
-            targetPosition = target.position + offset;
+            targetPosition = target.position + gm.offset;
         }
         else if (move.isFaceRight)
         {
-            targetPosition.x += xOffset;
+            targetPosition.x += gm.xOffset;
         }
         else
         {
-            targetPosition.x -= xOffset;
+            targetPosition.x -= gm.xOffset;
         }
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, gm.smoothness * Time.deltaTime);
         transform.position = smoothPosition;
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftBound, rightBound), Mathf.Clamp(transform.position.y, bottomBound, topBound), transform.position.z);
-    }
-
-    void OnDrawGizmos()
-    {
-        //drawing boundaries for camera
-        Gizmos.color = Color.green;
-
-        //top line
-        Gizmos.DrawLine(new Vector2(leftBound, topBound), new Vector2(rightBound, topBound));
-        //right line
-        Gizmos.DrawLine(new Vector2(rightBound, topBound), new Vector2(rightBound, bottomBound));
-        //bottom line
-        Gizmos.DrawLine(new Vector2(rightBound, bottomBound), new Vector2(leftBound, bottomBound));
-        //left line
-        Gizmos.DrawLine(new Vector2(leftBound, bottomBound), new Vector2(leftBound, topBound));
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, gm.leftBound, gm.rightBound), Mathf.Clamp(transform.position.y, gm.bottomBound, gm.topBound), transform.position.z);
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Data for camera
+    public Vector3 offset;
+    //offset for the front of the player
+    public float xOffset;
+    [Range(1, 10)]
+    public float smoothness;
+
+    //set bounds for the camera
+    public float leftBound;
+    public float rightBound;
+    public float topBound;
+    public float bottomBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //drawing lines for camera
+    void OnDrawGizmos()
+    {
+        //drawing boundaries for camera
+        Gizmos.color = Color.green;
+
+        //top line
+        Gizmos.DrawLine(new Vector2(leftBound, topBound), new Vector2(rightBound, topBound));
+        //right line
+        Gizmos.DrawLine(new Vector2(rightBound, topBound), new Vector2(rightBound, bottomBound));
+        //bottom line
+        Gizmos.DrawLine(new Vector2(rightBound, bottomBound), new Vector2(leftBound, bottomBound));
+        //left line
+        Gizmos.DrawLine(new Vector2(leftBound, bottomBound), new Vector2(leftBound, topBound));
+    }
+}
