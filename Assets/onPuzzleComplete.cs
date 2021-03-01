e09a27e65d481297b95024959eb1f4403bd85f4e
Erol Boyar <eboyar@uci.edu>
2021-02-28 22:59:14 -0800

    solutionchecker fix
    thing i forgot to do before my last push

---
+++ Assets/onPuzzleComplete.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onPuzzleComplete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("test the thing!");
+        GameObject[] puzzle2Things = GameObject.FindGameObjectsWithTag(this.tag);
+        foreach (GameObject g in puzzle2Things)
+        {
+            if (g.GetComponent<solutionManager>() != null)
+            {
+                g.GetComponent<solutionManager>().revealTextIfSolved();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
